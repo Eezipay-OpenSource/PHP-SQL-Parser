50316b17c58f9ce003047fa29b6a5c3c31b6df89
Eaton <e.e.metabee@gmail.com>
2018-08-07 15:53:08 +0200

    Corrected selection size orientation for offset syntax
    The offset should be set after the OFFSET keyword, Unlike the comma method used by mysql

--- src/PHPSQLParser/builders/LimitBuilder.php
+++ src/PHPSQLParser/builders/LimitBuilder.php
@@ -53,7 +53,7 @@ use PHPSQLParser\exceptions\UnableToCreateSQLException;
 class LimitBuilder implements Builder {
 
     public function build(array $parsed) {
-        $sql = ($parsed['offset'] ? $parsed['offset'] . " OFFSET " : "") . $parsed['rowcount'];
+        $sql = $parsed['rowcount'] . ($parsed['offset'] ? " OFFSET " . $parsed['offset'] : "");
         if ($sql === "") {
             throw new UnableToCreateSQLException('LIMIT', 'rowcount', $parsed, 'rowcount');
         }
